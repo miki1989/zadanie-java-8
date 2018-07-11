fed545ccefeff3098c2a6100aa3b7636ef9ec04d
miki1989 <mikolajheller@gmail.com>
2018-07-11 13:44:07 +0200

    Task nr 8, create table with names and print them. Used for loop.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        String [] tablica = new String[3];
+        tablica[0] = "Jan";
+        tablica[1] = "Ania";
+        tablica[2] = "Michał";
+
+        for(i = 0; i < tablica.length; i++){
+            System.out.print(tablica[i] + " ");
+        }
+    }
+}
